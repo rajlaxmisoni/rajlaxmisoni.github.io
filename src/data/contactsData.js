ab4005e5099efd5d273a9f39af7c4dae196d6786
Kushagra Soni <soni.kushagra24@gmail.com>
2022-07-26 00:48:25 -0500

    updated homepage package.json

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "rajlaxmisoni2803@gmail.com",
-  phone: "+1-2247707435",
+  phone: "+1-XXX-XXX-7433",
   address: "Brunswick, Ohio - 44212",
   location: "https://goo.gl/maps/XpKRmwW9KUoavQZH6",
   sheetAPI: "",
